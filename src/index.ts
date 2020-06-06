abb8dd136a36076bcab195e6785edc6046f58a66
Bill <bglesias@gmail.com>
2020-06-06 01:41:55 -0400

    implement spongebob case

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 export default function SpongeBobCase(input: String): String {
-  return "";
+  let shouldCapitalize = false;
+
+  return [...input]
+    .map((character) => {
+      const upperCase = character.toUpperCase();
+      // if the item is a punctuation mark, space, emoji, etc, just return it
+      if (upperCase === character && !character.match(/[A-Z]/)) {
+        // the first letter in the next sentence should be lower case
+        shouldCapitalize = false;
+        return character;
+      }
+
+      shouldCapitalize = !shouldCapitalize;
+      // since we are flipping the flag above, we want the opposite condition to return true
+      return !shouldCapitalize ? upperCase : character.toLowerCase();
+    })
+    .join("");
 }
