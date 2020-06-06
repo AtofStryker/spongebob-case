6e9cb5669415464f34496e761c76ae126b25c724
Bill <bglesias@gmail.com>
2020-06-05 21:52:15 -0400

    inital commit of repository setup

---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import SpongeBobCase from './index'
+
+describe('SpongeBob Case' , () => {
+    it('Stubs out a test', () => {
+        SpongeBobCase("")
+        expect(true).toBe(true)
+    })
+})
